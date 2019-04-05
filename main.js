f7219ffac8ab1a1431b73b832545ce378b69ee47
Tristan Bennett <betriistan@gmail.com>
2019-04-05 03:34:48 -0500

    added spin functionality for about section h2s

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
-$(document).ready(function () {
+$(document).ready(function() {
     "use strict";
-    $(".navbar-toggle").on("click", function () {
+    $("#spin h2").css('display', 'none');
+    $(".navbar-toggle").on("click", function() {
         $(this).toggleClass("active");
     });
 
-    $(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth > 500) {
-            $("#media a").removeClass("btn-block");
-    } else {
-      $("#media a").addClass("btn-block");
-    }
+    function spin(element) {
+        $(this).css('display', 'inline-block');
+        $(element).fadeIn(300).delay(3000).fadeOut(300, function() {
+            $(this).css('display', 'none');
+            let next = $(this).next('h2');
+            console.log(next)
+            spin(next.length > 0 ? next : $(this).parent().children().first());
+        });
+    };
 
-});
+    spin("#one");
+
+    $(window).resize(function() {
+        let viewportWidth = $(window).width();
+        if (viewportWidth > 500) {
+            $("#media a").removeClass("btn-block");
+        } else {
+            $("#media a").addClass("btn-block");
+        }
+
+    });
+
 });
